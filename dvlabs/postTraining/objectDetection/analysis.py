ccccfb8260f1296a489c72b2a052c0469782d4b5
Varun Bal <varun.bal.work@gmail.com>
2022-11-29 17:22:52 +0530

    `mistake_view` now does not show images without mistake in grid

--- dvlabs/postTraining/objectDetection/analysis.py
+++ dvlabs/postTraining/objectDetection/analysis.py
@@ -58,8 +58,64 @@ class Analyse:
         if vid_writer is not None:
             vid_writer.release()
 
-    # Alias for grid view method
-    view_mistakes = grid_view
+    def view_mistakes(self, save_dir=None, grid_size=(1, 1), resolution=(1280, 720),
+                  maintain_ratio=True, filter_classes=[], iou_thres=1):
+
+        image_names = list(self.gt_annos.keys())
+
+        batch_size = grid_size[0] * grid_size[1]
+
+        resize_w, resize_h = round(resolution[0] / grid_size[0]), round(resolution[1] / grid_size[1])
+
+        vid_writer = None
+        if save_dir is not None:
+            vid_writer = get_vid_writer(os.path.join(save_dir, "grid_output.mp4"), 1,
+                                        (resize_w*grid_size[0], resize_h*grid_size[1]))
+
+        filtered_gt_annos = {}
+        filtered_pred_annos = {}
+        filtered_image_names = []
+
+        for img_name in image_names:
+            filtered_gt = self.filter_anno(self.gt_annos[img_name], self.pred_annos[img_name], filter_classes,
+                                           iou_thres)
+
+            filtered_pred = self.filter_anno(self.pred_annos[img_name], self.gt_annos[img_name], filter_classes,
+                                             iou_thres)
+
+            if (len(filtered_gt['objects']) is not 0) or (len(filtered_pred['objects']) is not 0):
+                filtered_gt_annos[img_name] = filtered_gt
+                filtered_pred_annos[img_name] = filtered_pred
+                filtered_image_names.append(img_name)
+
+        batches = get_batches(filtered_image_names, batch_size)
+
+        for batch in batches:
+            batch_imgs = []
+            for img_name in batch:
+                img_path = os.path.join(self.img_dir, img_name)
+                img = cv2.imread(img_path)
+
+                filtered_gt = filtered_gt_annos[img_name]
+                filtered_pred = filtered_pred_annos[img_name]
+
+                self.display_anno(img, filtered_gt, (0, 255, 0))
+                self.display_anno(img, filtered_pred, (0, 255, 255))
+
+                batch_imgs.append(img)
+
+            grid = create_grid(batch_imgs, grid_size, (resize_h, resize_w), maintain_ratio)
+
+            if vid_writer is not None:
+                vid_writer.write(grid)
+            else:
+                cv2.imshow('grid', grid)
+                key = cv2.waitKey(0)
+                if key == 27 or key == ord('q'):
+                    break
+
+        if vid_writer is not None:
+            vid_writer.release()
 
     def display_anno(self, img, img_anon, color=(0, 255, 0)):
 
@@ -252,7 +308,10 @@ if __name__ == "__main__":
     # print(pd_anno)
 
     pt_analyser = Analyse(gt_anno, pd_anno, img_path)
-    # pt_analyser.grid_view(grid_size=(3, 3), resolution=(1280, 720), filter_classes=[], iou_thres=.75, maintain_ratio=True)
+    # pt_analyser.grid_view(grid_size=(3, 3), resolution=(1280, 720), filter_classes=[], iou_thres=.75,
+    #                       maintain_ratio=True)
+    pt_analyser.view_mistakes(grid_size=(3, 3), resolution=(1280, 720), filter_classes=[], iou_thres=.75,
+                              maintain_ratio=True)
     # pt_analyser.avg_iou_per_sample()
     # pt_analyser.evaluate_metric(0.5)
-    pt_analyser.confusion_matrix(0.5)
+    # pt_analyser.confusion_matrix(0.5)
